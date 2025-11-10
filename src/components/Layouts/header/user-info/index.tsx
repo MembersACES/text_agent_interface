b38430349e970baaa1dff5328f520b04fd2b71a6
MembersACES <members@acesolutions.com.au>
2025-11-10 09:52:27 +0800

    Removed avatar in top right

--- src/components/Layouts/header/user-info/index.tsx
+++ src/components/Layouts/header/user-info/index.tsx
@@ -7,7 +7,6 @@ import {
   DropdownTrigger,
 } from "@/components/ui/dropdown";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { LogOutIcon, SettingsIcon, UserIcon } from "./icons";
@@ -70,28 +69,18 @@ export function UserInfo() {
       <DropdownTrigger className="rounded align-middle outline-none ring-primary ring-offset-2 focus-visible:ring-1 dark:ring-offset-gray-dark">
         <span className="sr-only">My Account</span>
 
-        <figure className="flex items-center gap-3">
-          <Image
-            src={USER.image || "/images/user/user-03.png"}
-            className="size-12"
-            alt={`Avatar of ${USER.name}`}
-            role="presentation"
-            width={200}
-            height={200}
-          />
-          <figcaption className="flex items-center gap-1 font-medium text-dark dark:text-dark-6 max-[1024px]:sr-only">
-            <span>{isReauthenticating ? "Reauthenticating..." : USER.name}</span>
+        <div className="flex items-center gap-1 font-medium text-dark dark:text-dark-6">
+          <span>{isReauthenticating ? "Reauthenticating..." : USER.name}</span>
 
-            <ChevronUpIcon
-              aria-hidden
-              className={cn(
-                "rotate-180 transition-transform",
-                isOpen && "rotate-0",
-              )}
-              strokeWidth={1.5}
-            />
-          </figcaption>
-        </figure>
+          <ChevronUpIcon
+            aria-hidden
+            className={cn(
+              "rotate-180 transition-transform",
+              isOpen && "rotate-0",
+            )}
+            strokeWidth={1.5}
+          />
+        </div>
       </DropdownTrigger>
 
       <DropdownContent
@@ -100,24 +89,13 @@ export function UserInfo() {
       >
         <h2 className="sr-only">User information</h2>
 
-        <figure className="flex items-center gap-2.5 px-5 py-3.5">
-          <Image
-            src={USER.image || "/images/user/user-03.png"}
-            className="size-12"
-            alt={`Avatar for ${USER.name}`}
-            role="presentation"
-            width={200}
-            height={200}
-          />
+        <div className="px-5 py-3.5 space-y-1">
+          <div className="mb-2 text-base font-medium leading-none text-dark dark:text-white">
+            {isReauthenticating ? "Reauthenticating..." : USER.name}
+          </div>
 
-          <figcaption className="space-y-1 text-base font-medium">
-            <div className="mb-2 leading-none text-dark dark:text-white">
-              {isReauthenticating ? "Reauthenticating..." : USER.name}
-            </div>
-
-            <div className="leading-none text-gray-6">{USER.email}</div>
-          </figcaption>
-        </figure>
+          <div className="text-base leading-none text-gray-6">{USER.email}</div>
+        </div>
 
         <hr className="border-[#E8E8E8] dark:border-dark-3" />
 
